22332617ac3d6ce79a9cb48980e3e0624032891c
jhs512 <jangka512@gmail.com>
2025-01-16 11:20:22 +0900

    Product created

---
+++ backend/src/main/java/com/ll/domain/product/product/entity/Product.java
@@ -0,0 +1,23 @@
+package com.ll.domain.product.product.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import lombok.*;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Product {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+}
